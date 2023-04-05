444e145604cec7f3e863e787d4f7bca1ce999565
Andrew Rafalsky <andraffal@gmail.com>
2023-04-05 14:52:44 +0300

    feat: add mongoose

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT);
 }
 bootstrap();
